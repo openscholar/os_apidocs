5820be032d3d5bc8c4f5738458ea608d976aa706
Roy Segall <roy.segall@gizra.com>
2015-05-19 14:41:34 +0300

    Updates

--- api_project.js
+++ api_project.js
@@ -1,12 +1,18 @@
 define({
-  "name": "jquery-plugin",
-  "version": "0.0.0-ignored",
-  "description": "",
-  "sampleUrl": false,
+  "name": "OpenScholar Restful documentation",
+  "version": "0.1.0",
+  "description": "This is an example on how to interact with OpenScholar Restful end point.",
+  "title": "Custom apiDoc browser title",
+  "url": "http://localhost/OpenScholarMake/www/",
+  "sampleUrl": "http://localhost/OpenScholarMake/www/",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-05-18T06:07:02.268Z",
+    "time": "2015-05-19T08:42:19.230Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
