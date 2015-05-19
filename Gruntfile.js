a458e255be0fc8b3069fe887f23afe4edb28d17f
Roy Segall <roy.segall@gizra.com>
2015-05-19 11:43:19 +0300

    Pushing work.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,38 +5,29 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      src: {
-        files: '<%= jshint.src.src %>',
-        tasks: ['jshint:src', 'qunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'qunit']
-      }
-    },
     apidoc: {
       mypp: {
         src: grunt.file.readJSON('os_apidoc.json').path,
         dest: "doc/",
         options: {
-          includeFilters: [ ".*\\.class\\.php$" ],
-          excludeFilters: [ "node_modules/" ]
+          includeFilters: [ ".*\\.class\\.php$" ]
         }
       }
+    },
+    'gh-pages': {
+      options: {
+        base: 'doc'
+      },
+      src: ['**']
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-apidoc');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   // Default task.
   grunt.registerTask('generate', ['apidoc']);
-  grunt.registerTask('serve', ['generate', 'watch']);
+  grunt.registerTask('deploy', ['gh-pages']);
 
 };
